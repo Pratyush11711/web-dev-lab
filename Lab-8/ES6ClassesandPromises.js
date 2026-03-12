35d89e8bba52f92ac23c6b084ea7ae9df556066f
Pratyush11711 <pratyush.epi.spn117@gmail.com>
2026-03-12 10:04:54 +0530

    Lab-8

---
+++ Lab-8/ES6ClassesandPromises.js
@@ -0,0 +1,33 @@
+// ES6 Class
+class Course {
+    constructor(courseName, instructor) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+    }
+
+    displayCourse() {
+        console.log(`Course: ${this.courseName}, Instructor: ${this.instructor}`);
+    }
+}
+
+// Create object
+let course1 = new Course("Web Technologies", "Dr. Kumar");
+
+// Display course details
+course1.displayCourse();
+
+// Promise for enrollment
+let enrollCourse = new Promise((resolve, reject) => {
+    let seatsAvailable = true;
+
+    if (seatsAvailable) {
+        resolve("Enrollment Successful");
+    } else {
+        reject("Course Full");
+    }
+});
+
+// Handle promise result
+enrollCourse
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
